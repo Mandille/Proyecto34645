396f94d961eb7d8f730ca61f4879f2a01c9fcfc1
Mandille <mandillelaboral@gmail.com>
2022-12-17 21:09:04 -0300

    nuevos def

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -9,3 +9,19 @@ def curso(request):
     curso1.save()
     cadena_texto=f"curso guardado: Nombre: {curso1.nombre}, comision: {curso1.comision}"
     return HttpResponse(cadena_texto)
+
+ def cursos(request):
+    return HttpResponse ("Esto es una vista de cursos")
+
+ def estudiantes(request):
+    return HttpResponse ("Esto es una vista de estudiantes")
+
+ def profesores(request):
+    return HttpResponse ("Esto es una vista de profesores")
+
+ def entregables(request):
+    return HttpResponse ("Esto es una vista de entregables")
+
+ def inicio(request):
+    return HttpResponse ("Estoy en inicio!")
+
